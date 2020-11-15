ec65861b2b94e79f220a0039f70af5f85c0161a6
LaisMSantana <lais.santana123@hotmail.com>
2020-11-15 16:26:23 -0300

    Tela Conteudo

--- JuntosPorElesFrontend/src/app/modules/informativo/informativo-routing.module.ts
+++ JuntosPorElesFrontend/src/app/modules/informativo/informativo-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ConteudoComponent } from './conteudo/conteudo.component';
 import { InformativoComponent } from './informativo/informativo.component';
 
 const routes: Routes = [
   { path: '', component: InformativoComponent},
+  { path: 'conteudo', component: ConteudoComponent},
 ];
 
 @NgModule({
--- JuntosPorElesFrontend/src/app/modules/informativo/informativo.module.ts
+++ JuntosPorElesFrontend/src/app/modules/informativo/informativo.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { InformativoRoutingModule } from './informativo-routing.module';
 import { InformativoComponent } from './informativo/informativo.component';
+import { ConteudoComponent } from './conteudo/conteudo.component';
 
 
 @NgModule({
-  declarations: [InformativoComponent],
+  declarations: [InformativoComponent, ConteudoComponent],
   imports: [
     CommonModule,
     InformativoRoutingModule
